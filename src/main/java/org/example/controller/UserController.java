a5b2910e8475496a7e76e207e3771098fc5247ce
dmitars <dmitriy.tarasienko@gmail.com>
2025-04-29 21:13:17 +0300

    Initial commit

---
+++ src/main/java/org/example/controller/UserController.java
@@ -0,0 +1,39 @@
+package org.example.controller;
+
+import org.example.dao.JdbcUserDao;
+import org.example.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private JdbcUserDao jdbcUserDao;
+
+    @GetMapping
+    public String getUsers(Model model) {
+        model.addAttribute("users", jdbcUserDao.findAll());
+        return "user-list";
+    }
+
+    @GetMapping("/{id}")
+    public String getUser(Model model, @PathVariable("id") Integer id) {
+        model.addAttribute("user", jdbcUserDao.findById(id));
+        return "user-details";
+    }
+
+    @GetMapping("/add")
+    public String getAddUserPage(Model model) {
+        return "user-add";
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(@ModelAttribute User user){
+        jdbcUserDao.create(user);
+        return "redirect:/users";
+    }
+}
